26fad9a375157afd51472cd648b2507ae62d4ec5
wso <wso@CA-50-0963.corp.collectamerica.com>
2016-06-23 16:22:01 -0600

    Implemented connectin to 06

--- WebContent/nest/soaservices/Services.js
+++ WebContent/nest/soaservices/Services.js
@@ -21,30 +21,44 @@ servicesModule.controller('ServicesCtrl',
 	});
 
 
+//servicesModule.factory('AccountDispositionSvc',['$resource',
+//    function($resource){
+//    	return $resource("http://jsonplaceholder.typicode.com/posts", {},{
+//    		save: {method: 'POST', cache: false, isArray: false},
+//    	});
+//}]); 
+     
+
 servicesModule.factory('AccountDispositionSvc',['$resource',
-    function($resource){
-    	return $resource("http://jsonplaceholder.typicode.com/posts", {},{
-    		get: {method: 'GET', cache: false, isArray: true},
-    		save: {method: 'POST', cache: false, isArray: false},
-    	});
+function($resource){
+	return $resource("http://el01cn06:8002/AccountOSBRest/ProxyServices/AccountDisposition", {},{
+		save: {method: 'POST', cache: false, isArray: false},
+	});
 }]); 
-                                                	
+
                                                 	
 servicesModule.controller('AccountDispositionCtrl',
    function(AccountDispositionSvc){	
 	  var disp = this;
 	  disp.svc = AccountDispositionSvc;
 	  disp.text=  "Account Disposition Page";
+	  disp.operation='Add';
+	  
 	  disp.process = function(){		  
 			alert("Service Call successful");
-			var data = "id=" + disp.accountId + "&title=" + disp.dispositionCode + "&userId=1";
+			var data = "accountId=" + disp.accountId + "&disposition=" + disp.dispositionCode + "&operation=REM" + "&userName=NestUser";			
 			disp.svc.save({data: data},
-			function success(response){
-				console.log("Success:" + JSON.stringify(response));
+			
+					
+		   function success(response){
+				//console.log("Success:" + JSON.stringify(response));
 				disp.postEntries = response;
 			},
 			function error(errorResponse) {
-				console.log("Error:" + JSON.stringfy(errorResponse));
+				alert("Error happened");
+				console.log("Error happened");
+				
+				//console.log("Error:" + JSON.stringfy(errorResponse));
 			}
 			);	
 	  }	
