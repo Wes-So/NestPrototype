f350c9981d65c966e1b6804a43730b7b9d4532fd
Wes-So <wilsonfso@gmail.com>
2016-08-08 21:37:18 -0600

    Refactored logging service

--- WebContent/nest/logging/logging.js
+++ WebContent/nest/logging/logging.js
@@ -1,10 +1,31 @@
-var loggingModule = angular.module('Nest.Logging',[]);
+var loggingModule = angular.module('Nest.Logging',['ngResource']);
+
+loggingModule.factory('SmartLoggingSvc',['$resource',
+function($resource){
+	return $resource("http://jsonplaceholder.typicode.com/posts", {},{
+		save: {method: 'POST', cache: false, isArray: false},
+    });
+}]); 
 
 
-loggingModule.factory('LogServiceCall',function(){ 
+
+
+loggingModule.factory('LogServiceCall',function(SmartLoggingSvc){ 
 	return{
 		log: function(data){ 
 			console.log("Logging service called " + data);
+			 
+			
+			SmartLoggingSvc.save({data: data},		
+			
+			function success(response){
+				console.log("Success:" + JSON.stringify(response));				
+			},
+			function error(errorResponse) { 
+				console.log("Error:" + JSON.stringfy(errorResponse));
+			}
+			);	 
+			
 		}
 	}
  });
